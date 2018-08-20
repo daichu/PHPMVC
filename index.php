86dff4c187c09fc0cff55062571a6b2015e3ffad
daichu <daik6th1@gmail.com>
2018-08-20 16:38:52 +0700

    index3 phpmvc

--- index.php
+++ index.php
@@ -5,4 +5,4 @@
  * Date: 8/20/2018
  * Time: 4:29 PM
  */
-echo "chào bạn";
+echo "chào bạn đây là trang index";
--- index3.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: CQD
- * Date: 8/20/2018
- * Time: 4:34 PM
- */
-echo "đây là index3";
